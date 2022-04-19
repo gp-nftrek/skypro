d3dc2b43ed28c4bd946fa9d7485295799757c5ce
babk1nvnuk <babkinartem20@gmail.com>
2022-04-19 22:40:13 +0200

    1

---
+++ src/skypro/Main.java
@@ -0,0 +1,45 @@
+package skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        6
+//        System.out.println();
+//        int salary = 100000;
+//        int age = 25;
+//        double limit19 =salary*3;
+//        double limit18=salary*2;
+//
+//        if (salary >= 50000 && salary<80000) {
+//            limit18 = limit18*1.2;
+//            limit19=limit19*1.2; }
+//        if(salary>=80000){
+//            limit18 = limit18*1.5;
+//            limit19=limit19*1.5;}
+//        if (age >= 23) {
+//            System.out.println("Mi gotovy vydat vam kreditnuy katy c limitom " + limit19 + " rub");
+//        } else {
+//            System.out.println("Mi gotovy vydat vam kreditnuy katy c limitom " + limit18 + " rub");
+//        }
+//        7
+        int age = 25;
+        int salary = 90000;
+        double wantedSum = 330000;
+        double interestRate = 1.1;
+
+        if (age < 23) {
+            interestRate = interestRate + 0.01;
+        }
+        if (age < 30 && age >= 23) {
+            interestRate = interestRate + 0.005;
+        }
+        if (salary > 80000) {
+            interestRate = interestRate - 0.007;
+        }
+        if ((wantedSum * interestRate) / 12 > salary / 2) {
+            System.out.println("OTKAZ");
+        } else {
+            System.out.println("ODOBRENO. " + "Max platej pri zp " + salary + " raven " + salary / 2 + "rub. Platej po kreditu " + (wantedSum * interestRate) / 12 + " rub.");
+        }
+    }
+}
